15192889193aaf89422bf2b499960c9cd984284f
NMVikings <no.more.vikings@gmail.com>
2018-12-15 21:51:14 +0300

    Refactor tests a little bit

--- src/day1/__tests__/calculateResultFrequency.test.ts
+++ src/day1/__tests__/calculateResultFrequency.test.ts
@@ -1,34 +1,36 @@
 import getTestInput from "../../utils/getTestInput";
+
 import calculateResultFrequency from "../calculateResultFrequency";
 
-const partOneTestsPath = __dirname + "/__fixtures__/Part One";
+describe("test calculateResultFrequency", () => {
+  const partOneTestsPath = __dirname + "/__fixtures__/Part One";
+  const tests = [
+    {
+      expectedResult: 3,
+      fixtureName: `input1.txt`,
+      name: "input1"
+    },
+    {
+      expectedResult: 0,
+      fixtureName: `input2.txt`,
+      name: "input2"
+    },
+    {
+      expectedResult: -6,
+      fixtureName: `input3.txt`,
+      name: "input3"
+    },
+    {
+      expectedResult: 529,
+      fixtureName: `challenge.txt`,
+      name: "challenge"
+    }
+  ];
 
-const tests = [
-  {
-    expectedResult: 3,
-    fixtureName: `input1.txt`,
-    name: "input1"
-  },
-  {
-    expectedResult: 0,
-    fixtureName: `input2.txt`,
-    name: "input2"
-  },
-  {
-    expectedResult: -6,
-    fixtureName: `input3.txt`,
-    name: "input3"
-  },
-  {
-    expectedResult: 529,
-    fixtureName: `challenge.txt`,
-    name: "challenge"
-  }
-];
-
-tests.forEach(({ name, fixtureName, expectedResult }) => {
-  it(name, () => {
-    const input = getTestInput(`${partOneTestsPath}/${fixtureName}`);
-    expect(calculateResultFrequency(input)).toBe(expectedResult);
+  tests.forEach(({ name, fixtureName, expectedResult }) => {
+    it(name, () => {
+      const input = getTestInput(`${partOneTestsPath}/${fixtureName}`);
+      expect(calculateResultFrequency(input)).toBe(expectedResult);
+    });
   });
 });
--- src/day1/__tests__/findFirstFrequencyReachesTwice.test.ts
+++ src/day1/__tests__/findFirstFrequencyReachesTwice.test.ts
@@ -1,40 +1,41 @@
-import findFirstFrequencyReachesTwice from "../findFirstFrequencyReachesTwice";
-
 import getTestInput from "../../utils/getTestInput";
 
-const partOneTestsPath = __dirname + "/__fixtures__/Part Two";
+import findFirstFrequencyReachesTwice from "../findFirstFrequencyReachesTwice";
 
-const tests = [
-  {
-    expectedResult: 0,
-    fixtureName: `input1.txt`,
-    name: "input1"
-  },
-  {
-    expectedResult: 10,
-    fixtureName: `input2.txt`,
-    name: "input2"
-  },
-  {
-    expectedResult: 5,
-    fixtureName: `input3.txt`,
-    name: "input3"
-  },
-  {
-    expectedResult: 14,
-    fixtureName: `input4.txt`,
-    name: "input4"
-  },
-  {
-    expectedResult: 464,
-    fixtureName: `challenge.txt`,
-    name: "challenge"
-  }
-];
+describe("test findFirstFrequencyReachesTwice", () => {
+  const partTwoTestsPath = __dirname + "/__fixtures__/Part Two";
+  const tests = [
+    {
+      expectedResult: 0,
+      fixtureName: `input1.txt`,
+      name: "input1"
+    },
+    {
+      expectedResult: 10,
+      fixtureName: `input2.txt`,
+      name: "input2"
+    },
+    {
+      expectedResult: 5,
+      fixtureName: `input3.txt`,
+      name: "input3"
+    },
+    {
+      expectedResult: 14,
+      fixtureName: `input4.txt`,
+      name: "input4"
+    },
+    {
+      expectedResult: 464,
+      fixtureName: `challenge.txt`,
+      name: "challenge"
+    }
+  ];
 
-tests.forEach(({ name, fixtureName, expectedResult }) => {
-  it(name, () => {
-    const input = getTestInput(`${partOneTestsPath}/${fixtureName}`);
-    expect(findFirstFrequencyReachesTwice(input)).toBe(expectedResult);
+  tests.forEach(({ name, fixtureName, expectedResult }) => {
+    it(name, () => {
+      const input = getTestInput(`${partTwoTestsPath}/${fixtureName}`);
+      expect(findFirstFrequencyReachesTwice(input)).toBe(expectedResult);
+    });
   });
 });
